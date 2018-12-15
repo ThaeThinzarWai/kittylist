8a0e4be3bfda09dc093a6b9404b9616630add417
ThaeThae <thaetinzar.wai@innov8te.com.sg>
2018-12-15 15:58:40 +0630

    call all cats api

--- src/containers/CatList.js
+++ src/containers/CatList.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 class CatList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            cats : []
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://api.thecatapi.com/v1/images/?limit=10", {
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'x-api-key': '6e27939c-67e0-4eaa-bb9e-9f9e2a212c9b'
+            })
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        cats : result
+                    });
+                }
+            )
+    }
+
     render() {
+        const cats = this.state.cats;
+
         return (
             <div>
                 <div className="row">
-                    <div className="col-md-12">
-                        <h2>Welcome From Cat List Page</h2>
-                    </div>
+                    {cats.map(cat => (
+                        <div className="col-md-4">
+                            <h2>{cat.sub_id}</h2>
+                        </div>
+                    ))}
                 </div>
             </div>
         );
